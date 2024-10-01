470414a183b214f91e9ba3eb3f07c6ecf43cb723
Camila2015 <167145122+Camila2015@users.noreply.github.com>
2024-09-30 22:00:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,17 @@ import base64
 
 st.title("Conversión de Texto a Audio")
 
-# Cambiar imagen por Caperucita-Roja.jpg
 image = Image.open('Caperucita-Roja.jpg')
 st.image(image, width=350)
 
 with st.sidebar:
     st.subheader("Escribe y/o selecciona texto para ser escuchado.")
 
-# Crear carpeta temporal si no existe
 try:
     os.mkdir("temp")
 except:
     pass
 
-# Texto de la fábula de Caperucita Roja
 st.subheader("La fábula de Caperucita Roja")
 st.write('''Había una vez una niña muy bonita. Su madre le había hecho una pequeña capa roja, 
     y como le sentaba tan bien, todos la llamaban Caperucita Roja. Un día su madre le pidió que 
@@ -33,10 +30,8 @@ st.write('''Había una vez una niña muy bonita. Su madre le había hecho una pe
 
 st.markdown(f"¿Quieres escucharlo? Copia el texto a continuación para convertirlo en audio.")
 
-# Campo de texto para que el usuario copie la fábula u otro texto
 text = st.text_area("Ingrese el texto a escuchar.")
 
-# Selección del lenguaje
 option_lang = st.selectbox(
     "Selecciona el lenguaje",
     ("Español", "English")
@@ -44,17 +39,15 @@ option_lang = st.selectbox(
 
 lg = 'es' if option_lang == "Español" else 'en'
 
-# Función para convertir texto a audio
 def text_to_speech(text, lg):
     tts = gTTS(text, lang=lg)
     try:
-        my_file_name = text[0:20]  # Nombre del archivo con los primeros 20 caracteres del texto
+        my_file_name = text[0:20]
     except:
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name
 
-# Definir la animación con CSS
 def add_custom_css():
     custom_css = """
     <style>
@@ -64,7 +57,7 @@ def add_custom_css():
         border-top: 8px solid #3498db;
         width: 60px;
         height: 60px;
-        -webkit-animation: spin 2s linear infinite; /* Safari */
+        -webkit-animation: spin 2s linear infinite;
         animation: spin 2s linear infinite;
         margin: auto;
     }
@@ -83,23 +76,19 @@ def add_custom_css():
     """
     st.markdown(custom_css, unsafe_allow_html=True)
 
-# Mostrar la animación al convertir el texto a audio
 if st.button("Convertir a Audio"):
-    add_custom_css()  # Añadir la animación CSS
-    st.markdown('<div class="spinner"></div>', unsafe_allow_html=True)  # Mostrar la animación
+    add_custom_css()
+    st.markdown('<div class="spinner"></div>', unsafe_allow_html=True)
 
-    time.sleep(2)  # Simula el tiempo de procesamiento
+    time.sleep(2)
 
-    # Convertir el texto a audio
     result = text_to_speech(text, lg)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
     
-    # Mostrar el audio
     st.markdown(f"## Tú audio:")
     st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
-# Función para eliminar archivos antiguos
 def remove_files(n):
     mp3_files = glob.glob("temp/*mp3")
     if len(mp3_files) != 0:
@@ -110,5 +99,5 @@ def remove_files(n):
                 os.remove(f)
                 print("Deleted ", f)
 
-# Eliminar archivos después de 7 días
 remove_files(7)
+
